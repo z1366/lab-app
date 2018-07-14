3af045ab509b1727ea5106e3e9e1a679118afcd5
Дмитрий Борисов <z1365@mail.ru>
2018-07-14 05:21:00 +0300

    Теперь пункты списка выбираются, показывается детальная информация о герое. Пока и информация и список в одном компоненте.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import {Hero} from '../hero.model';
 import { HEROES } from '../mock-heroes';
+import {Hero} from '../hero.model';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +8,12 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {id: 1, name: 'Человек-пукан'};
   heroes = HEROES;
+  selectedHero: Hero;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
