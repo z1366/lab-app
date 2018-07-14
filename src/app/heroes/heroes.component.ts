dcc4b459608de0b68636167838345bc8391d5090
Дмитрий Борисов <z1365@mail.ru>
2018-07-14 04:27:50 +0300

    Сделана модель героя. Фейковый массив героев. Во вью вывожу список через *ngFor.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+// import {Hero} from '../hero.model';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // hero: Hero = {id: 1, name: 'Человек-пукан'};
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
+
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero.model';
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Жопорыл' },
+  { id: 12, name: 'Нарко' },
+  { id: 13, name: 'Пиздобрюх' },
+  { id: 14, name: 'Человек-пукан' },
+  { id: 15, name: 'Баба Клава' },
+  { id: 16, name: 'Человек-рубероид' },
+  { id: 17, name: 'Динама' },
+  { id: 18, name: 'Доктор Фу' },
+  { id: 19, name: 'Мамба' },
+  { id: 20, name: 'Тырнада' }
+];
